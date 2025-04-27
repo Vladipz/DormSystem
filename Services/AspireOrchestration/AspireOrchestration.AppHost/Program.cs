ebbc6f520dc6232f50bbfbcc5d573eabb6c8304b
Vladyslav Danilchuk <vladdanilchuk9@gmail.com>
2025-04-27 13:44:58 +0300

    feat(rooms): integrate Rooms.API with AspireOrchestration

--- Services/AspireOrchestration/AspireOrchestration.AppHost/Program.cs
+++ Services/AspireOrchestration/AspireOrchestration.AppHost/Program.cs
@@ -4,8 +4,11 @@ var authService = builder.AddProject<Projects.Auth_API>("auth-service");
 
 var eventService = builder.AddProject<Projects.Events_API>("event-service");
 
+var roomService = builder.AddProject<Projects.Rooms_API>("room-service");
+
 var apiGateway = builder.AddProject<Projects.ApiGateway_YARP>("api-gateway")
     .WithReference(authService)
-    .WithReference(eventService);
+    .WithReference(eventService)
+    .WithReference(roomService);
 
 builder.Build().Run();
