35ada51b34ab532209ef46918c348778b957b042
Vladyslav Danilchuk <vladdanilchuk9@gmail.com>
2025-05-22 02:38:34 +0300

    feat(avatar): add logo and avatar component to AppSidebar

--- frontend/dorm-app/src/components/AppSidebar.tsx
+++ frontend/dorm-app/src/components/AppSidebar.tsx
@@ -8,6 +8,7 @@ import {
   ShoppingCart,
   Users,
 } from "lucide-react";
+import { Avatar, AvatarFallback, AvatarImage } from "./ui";
 
 const navigationItems = [
   { path: "/", icon: Home, label: "Overview" },
@@ -50,13 +51,22 @@ export function AppSidebar() {
   const isRouteActive = (path: string) => {
     return location.pathname === path;
   };
-
   return (
     <div className="bg-card hidden w-64 border-r p-4 shadow-lg md:block">
       <div className="flex h-full flex-col">
-        <div className="mb-8 flex items-center">
-          <Home className="text-primary mr-2 h-6 w-6" />
-          <h1 className="text-xl font-bold">Dorm</h1>
+        <div className="mb-8 ml-8 flex items-center justify-start gap-2.5">
+          <Avatar>
+            <AvatarImage
+              src="/WhiteLogo.svg"
+              alt="InDorm Logo"
+              className="h-8 w-8"
+            />
+
+            <AvatarFallback className="bg-primary/10 text-primary/80">
+              inDorm
+            </AvatarFallback>
+          </Avatar>
+          <h1 className="text-xl font-bold">InDorm</h1>
         </div>
 
         <nav className="flex-1 space-y-2">
