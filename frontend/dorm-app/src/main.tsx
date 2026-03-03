582ab3319a8fb3047115d0e873ee0de9a9e0f908
Vladyslav Danilchuk <vladdanilchuk9@gmail.com>
2026-03-03 23:06:55 +0200

    i forget to commit

--- frontend/dorm-app/src/main.tsx
+++ frontend/dorm-app/src/main.tsx
@@ -1,19 +1,9 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { createRouter, RouterProvider } from "@tanstack/react-router";
+import { RouterProvider } from "@tanstack/react-router";
 import { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import { routeTree } from "./routeTree.gen";
-
-// Create a new router instance
-const router = createRouter({ routeTree });
-
-// Register the router instance for type safety
-declare module "@tanstack/react-router" {
-  interface Register {
-    router: typeof router;
-  }
-}
+import { router } from "./router";
 
 const queryClient = new QueryClient({
   defaultOptions: {
